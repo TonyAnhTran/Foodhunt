f32dff95e63bdea54428ef96b305baeb1d5e8e45
Tu (Tony) A. TRAN <tu.tran@enclave.vn>
2015-06-01 14:24:53 +0700

    Add login flow

---
+++ Foodhunt/LoginViewController/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Foodhunt
+//
+//  Created by Tu (Tony) A. TRAN on 6/1/15.
+//  Copyright (c) 2015 TW.FLcers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
