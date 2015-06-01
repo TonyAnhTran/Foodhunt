901eb04ff580c8b6f643dd82eddab17b53df80a0
Tu (Tony) A. TRAN <tu.tran@enclave.vn>
2015-06-01 08:27:46 +0700

    Initial Commit

---
+++ Foodhunt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Foodhunt
+//
+//  Created by Tu (Tony) A. TRAN on 6/1/15.
+//  Copyright (c) 2015 TW.FLcers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Foodhunt/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  Foodhunt
+//
+//  Created by Tu (Tony) A. TRAN on 6/1/15.
+//  Copyright (c) 2015 TW.FLcers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
